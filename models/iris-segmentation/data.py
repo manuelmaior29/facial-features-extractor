774b76e0dd91a9b147c562baf4d37de27b086bb4
manuelmaior29 <manuelmaior.29@gmail.com>
2023-10-16 20:35:01 +0300

    Update data.py
    Made minor fixes

--- models/iris-segmentation/data.py
+++ models/iris-segmentation/data.py
@@ -12,16 +12,16 @@ class IrisDataset(Dataset):
 class CASIAIris(IrisDataset):
     def __init__(self, root, transform=None):
         super().__init__(root, transform)
-        self.ipts = os.listdir(f'{self.root}\\{self.sub_dir}\\input')
-        self.tgts = os.listdir(f'{self.root}\\{self.sub_dir}\\segmentation')
+        self.ipts = sorted(os.listdir(f'{self.root}/input'))
+        self.tgts = sorted(os.listdir(f'{self.root}/segmentation'))
         
     def __len__(self):
         return len(self.ipts)
     
     def __getitem__(self, index):
         try:
-            ipt = Image.open(f'{self.ipts[index]}')
-            tgt = Image.open(f'{self.tgts[index]}')
+            ipt = Image.open(f'{self.root}/input/{self.ipts[index]}')
+            tgt = Image.open(f'{self.root}/segmentation/{self.tgts[index]}')
             if self.transform:
                 ipt, tgt = self.transform(ipt, tgt)
             return ipt, tgt
