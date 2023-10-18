64dc974cb6edaf7b7e07108bbca0520f1fef9b72
manuelmaior29 <manuelmaior.29@gmail.com>
2023-10-18 19:51:41 +0300

    Update data.py
    Mapped label values and adapted data format for training

--- models/iris-segmentation/data.py
+++ models/iris-segmentation/data.py
@@ -3,6 +3,7 @@ import os
 from torchvision import transforms
 from torch.utils.data import Dataset
 from PIL import Image
+from utils.ext_transforms import ExtToTensor
 
 class IrisDataset(Dataset):
     def __init__(self, root, transform=None):
@@ -21,10 +22,13 @@ class CASIAIris(IrisDataset):
     def __getitem__(self, index):
         try:
             ipt = Image.open(f'{self.root}/input/{self.ipts[index]}')
-            tgt = Image.open(f'{self.root}/segmentation/{self.tgts[index]}')
+            tgt = Image.open(f'{self.root}/segmentation/{self.tgts[index]}').convert('L')
+            ipt, tgt = ExtToTensor()(ipt, tgt)
+            tgt[tgt == 255] = 1
             if self.transform:
                 ipt, tgt = self.transform(ipt, tgt)
-            return ipt, tgt
+                
+            return ipt, tgt.long()
         except:
             print(f'Could not get data pair at index {index}!')
     
