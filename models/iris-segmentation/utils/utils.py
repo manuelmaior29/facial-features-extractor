dca7e9173031fc63d9bf209ffd00082ec328cb29
manuelmaior29 <manuelmaior.29@gmail.com>
2023-10-20 22:10:27 +0300

    Update utils.py
    Made minor cmap change for show example pair util

--- models/iris-segmentation/utils/utils.py
+++ models/iris-segmentation/utils/utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 def show_example_pair(ipt_image, tgt_image):
     fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(ipt_image)
-    axs[1].imshow(tgt_image)
+    axs[0].imshow(ipt_image, cmap='gray')
+    axs[1].imshow(tgt_image, cmap='gray')
     fig.show()
+    plt.waitforbuttonpress(1)
