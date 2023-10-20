e513b004fda9bb5ffba4b77cd65c6da0ed14059c
manuelmaior29 <manuelmaior.29@gmail.com>
2023-10-20 22:10:09 +0300

    Update ext_transforms.py
    Added random cropping and compose custom transforms

--- models/iris-segmentation/utils/ext_transforms.py
+++ models/iris-segmentation/utils/ext_transforms.py
@@ -7,6 +7,23 @@ import numbers
 import numpy as np
 from PIL import Image
 
+class ExtCompose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, ipt, tgt):
+        for t in self.transforms:
+            ipt, tgt = t(ipt, tgt)
+        return ipt, tgt
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.transforms:
+            format_string += '\n'
+            format_string += '    {0}'.format(t)
+        format_string += '\n)'
+        return format_string
+
 class ExtToTensor(object):
     def __init__(self, normalize=True, target_type='uint8'):
         self.normalize = normalize
@@ -55,3 +72,16 @@ class ExtNormalize(object):
     
     def __repr__(self):
         return self.__class__.__name__ + f'(mean={self.mean}, std={self.std})'
+    
+class ExtRandomCrop(object):
+    def __init__(self, crop_size) -> None:
+        self.crop_size = crop_size
+
+    def __call__(self, ipt, tgt):
+        if ipt.shape[1] < self.crop_size or ipt.shape[2] < self.crop_size:
+            return ipt, tgt
+        pos_x = random.randint(0, ipt.shape[1] - self.crop_size)
+        pos_y = random.randint(0, ipt.shape[2] - self.crop_size)
+        return F.crop(ipt, pos_x, pos_y, self.crop_size, self.crop_size), F.crop(tgt, pos_x, pos_y, self.crop_size, self.crop_size)
+        
+        
