037396e0dd8aebc13386028a5752a6f1f381170a
manuelmaior29 <manuelmaior.29@gmail.com>
2023-10-21 18:02:15 +0300

    Updated train.py and utils.py
    Made fixes, implemented plot saving utility and added subset selection possibility

--- models/iris-segmentation/train.py
+++ models/iris-segmentation/train.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader, Subset
 import torch.optim as optim
 import torch.nn as nn
 import torchvision.models.segmentation.fcn as fcn
-import tqdm
+from tqdm import tqdm
 
 def train_epoch(model: nn.Module, dataloader: DataLoader, optimizer: optim.Optimizer,
                 criterion: nn.Module, device: str):
@@ -17,35 +17,41 @@ def train_epoch(model: nn.Module, dataloader: DataLoader, optimizer: optim.Optim
         loss.backward()
         optimizer.step()
         epoch_loss += [loss.item()]
-        print(loss.item())
-    return model, (epoch_loss / len(dataloader))
+    return model, (sum(epoch_loss) / len(dataloader))
 
 def train(model: nn.Module, dataloader: DataLoader, scheduler: optim.lr_scheduler._LRScheduler,
           criterion: nn.Module, device: str, epochs: int):
     model.train()
     model.to(device)
-    for _ in range(epochs):
+    epoch_losses = []
+    for _ in tqdm(range(epochs)):
         model, loss = train_epoch(model, dataloader, optimizer, criterion, device)
-        print(loss)
+        epoch_losses += [loss]
         scheduler.step()
+    save_simple_2d_plot(range(epochs), epoch_losses, title='Loss over epochs', xlabel='Epoch', ylabel='Loss')
         
 if __name__ == "__main__":
     
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     data_root = r'C:\Users\Manuel\Projects\GitHub_Repositories\facial-features-extractor\models\iris-segmentation\data\casia-iris-segmentation'
     transform = ExtCompose([ExtRandomCrop(crop_size=256), 
                             ExtRandomVerticalFlip(p=0.5), 
                             ExtRandomHorizontalFlip(p=0.5),
                             ExtNormalize(mean=[0.485], std=[0.225])])
-
+    random.seed(1)
+    
     iris_dataset_train = data.CASIAIris(root=data_root, transform=transform)
-    iris_dataset_train = 
+    iris_dataset_subset_size = 40
+    iris_dataset_indices = list(range(len(iris_dataset_train)))
+    
+    random.shuffle(iris_dataset_indices)
+    iris_dataset_train = Subset(iris_dataset_train, iris_dataset_indices[:iris_dataset_subset_size])
     iris_dataloader_train = DataLoader(iris_dataset_train, batch_size=4, shuffle=False)
     
     model = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=False, num_classes=2)
     model.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
 
-    train(model, iris_dataloader_train, scheduler, criterion, device, epochs=1)
+    train(model, iris_dataloader_train, scheduler, criterion, device, epochs=10)
--- models/iris-segmentation/utils/utils.py
+++ models/iris-segmentation/utils/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import time
 
 def show_example_pair(ipt_image, tgt_image):
     fig, axs = plt.subplots(1, 2)
@@ -6,3 +7,14 @@ def show_example_pair(ipt_image, tgt_image):
     axs[1].imshow(tgt_image, cmap='gray')
     fig.show()
     plt.waitforbuttonpress(1)
+
+def save_simple_2d_plot(x, y, title=None, xlabel=None, ylabel=None):
+    fig, axs = plt.subplots(1, 1)
+    axs.plot(x, y)
+    if title:
+        axs.set_title(title)
+    if xlabel:
+        axs.set_xlabel(xlabel)
+    if ylabel:
+        axs.set_ylabel(ylabel)
+    fig.savefig(f'{title}_{time.time().__repr__()}.png')
