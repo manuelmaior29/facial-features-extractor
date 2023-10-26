cd1ad58cd551289378e272b369abe295067b08fe
manuelmaior29 <manuelmaior.29@gmail.com>
2023-10-26 20:32:03 +0300

    Update train.py
    Added validation function
    Added parameter parsing
    Cleaned + small refactoring

--- models/iris-segmentation/train.py
+++ models/iris-segmentation/train.py
@@ -1,13 +1,26 @@
 from utils import *
 from PIL import Image
 from torch.utils.data import DataLoader, Subset
+from tqdm import tqdm
+from argparse import ArgumentParser
+import sys
 import torch.optim as optim
 import torch.nn as nn
 import torchvision.models.segmentation.fcn as fcn
-from tqdm import tqdm
+
+def validate(model: nn.Module, dataloader: DataLoader, metrics: SegmentationMetrics, device: str):
+    model.eval()
+    metrics.reset()
+    with torch.no_grad():
+        for ipt, tgt in dataloader:
+            iptd, tgtd = ipt.to(device), tgt.to(device)
+            pred = torch.argmax(model(iptd)['out'], dim=1).cpu()
+            metrics.update(pred, tgtd.cpu())
+    return metrics
 
 def train_epoch(model: nn.Module, dataloader: DataLoader, optimizer: optim.Optimizer,
                 criterion: nn.Module, device: str):
+    model.train()
     epoch_loss = []
     for ipt, tgt in dataloader:
         iptd, tgtd = ipt.to(device), tgt.to(device)
@@ -19,39 +32,60 @@ def train_epoch(model: nn.Module, dataloader: DataLoader, optimizer: optim.Optim
         epoch_loss += [loss.item()]
     return model, (sum(epoch_loss) / len(dataloader))
 
-def train(model: nn.Module, dataloader: DataLoader, scheduler: optim.lr_scheduler._LRScheduler,
-          criterion: nn.Module, device: str, epochs: int):
-    model.train()
+def train(model: nn.Module, dataloader_train: DataLoader, dataloader_val: DataLoader, scheduler: optim.lr_scheduler._LRScheduler,
+          optimizer: optim.Optimizer, criterion: nn.Module, metrics: SegmentationMetrics, device: str, epochs: int):
     model.to(device)
     epoch_losses = []
+    metric_results = []
     for _ in tqdm(range(epochs)):
-        model, loss = train_epoch(model, dataloader, optimizer, criterion, device)
+        model, loss = train_epoch(model, dataloader_train, optimizer, criterion, device)
         epoch_losses += [loss]
         scheduler.step()
+        validate(model, dataloader_val, metrics, device)
+        metric_results += [metrics.get_results()]
     save_simple_2d_plot(range(epochs), epoch_losses, title='Loss over epochs', xlabel='Epoch', ylabel='Loss')
-        
-if __name__ == "__main__":
-    
+    save_simple_2d_plot(range(epochs), [x['Mean IoU'] for x in metric_results], title='mIoU over epochs', xlabel='Epoch', ylabel='mIoU')
+
+def main():
+    # Parameters
+    parser = ArgumentParser()
+    parser.add_argument('--data_root', type=str, required=True)
+    parser.add_argument('--iris_train_subset_size', type=int, required=True)
+    parser.add_argument('--iris_val_subset_size', type=int, required=True)
+    parser.add_argument('--crop_size', type=int, required=True)
+    parser.add_argument('--epochs', type=int, required=True)
+    parser.add_argument('--lr', type=float, required=True)
+    parser.add_argument('--batch_size', type=int, required=True)
+    parser.add_argument('--seed', type=int, required=True)
+    args = parser.parse_args()
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    data_root = r'C:\Users\Manuel\Projects\GitHub_Repositories\facial-features-extractor\models\iris-segmentation\data\casia-iris-segmentation'
-    transform = ExtCompose([ExtRandomCrop(crop_size=256), 
-                            ExtRandomVerticalFlip(p=0.5), 
+    random.seed(args.seed)
+
+    # Dataset preparation
+    transform = ExtCompose([ExtRandomCrop(crop_size=args.crop_size),
+                            ExtRandomVerticalFlip(p=0.5),
                             ExtRandomHorizontalFlip(p=0.5),
                             ExtNormalize(mean=[0.485], std=[0.225])])
-    random.seed(1)
-    
-    iris_dataset_train = data.CASIAIris(root=data_root, transform=transform)
-    iris_dataset_subset_size = 40
-    iris_dataset_indices = list(range(len(iris_dataset_train)))
-    
+    iris_dataset = data.CASIAIris(root=args.data_root, transform=transform)
+    iris_dataset_indices = list(range(len(iris_dataset)))
     random.shuffle(iris_dataset_indices)
-    iris_dataset_train = Subset(iris_dataset_train, iris_dataset_indices[:iris_dataset_subset_size])
-    iris_dataloader_train = DataLoader(iris_dataset_train, batch_size=4, shuffle=False)
+    iris_dataset_indices_train = random.sample(iris_dataset_indices, args.iris_train_subset_size)
+    iris_dataset_indices_val = list(set(iris_dataset_indices) - set(iris_dataset_indices_train))[:args.iris_val_subset_size]
+    iris_dataset_train = Subset(iris_dataset, iris_dataset_indices_train)
+    iris_dataset_val = Subset(iris_dataset, iris_dataset_indices_val)
+    iris_dataloader_train = DataLoader(iris_dataset_train, batch_size=args.batch_size, shuffle=True)
+    iris_dataloader_val = DataLoader(iris_dataset_val, batch_size=1, shuffle=False)
     
+    # Model preparation
     model = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=False, num_classes=2)
     model.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+    metrics = SegmentationMetrics(num_classes=2)
 
-    train(model, iris_dataloader_train, scheduler, criterion, device, epochs=10)
+    train(model, iris_dataloader_train, iris_dataloader_val, scheduler, optimizer, criterion, metrics, device, epochs=args.epochs)
+
+if __name__ == '__main__':
+    main()
--- models/iris-segmentation/utils/__init__.py
+++ models/iris-segmentation/utils/__init__.py
@@ -1,3 +1,4 @@
 import data
 from utils.ext_transforms import *
 from utils.utils import *
+from utils.metrics import *
